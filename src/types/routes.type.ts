2011ff04ffff7901d8e0ee8c81b4060a75c11c33
Ayon203008 <paypal572874@gmail.com>
2026-01-18 20:33:01 +0600

    types added

---
+++ src/types/routes.type.ts
@@ -0,0 +1,20 @@
+// export const adminRoutes = [
+//     {
+//         title: "User Management",
+//         items: [
+//             {
+//                 title: "Analytics",
+//                 url: "/analytics"
+//             }
+//         ]
+//     }
+// ]
+
+
+export interface Route {
+    title:string,
+    items:{
+        title:string;
+        url:string
+    }[]
+}
