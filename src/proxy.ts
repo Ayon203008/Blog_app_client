3e521572245a523f4a4f67cbc47f5b31dc0616b0
Ayon203008 <paypal572874@gmail.com>
2026-01-22 18:32:37 +0600

    protacted route added in proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,38 @@
 import { NextRequest, NextResponse } from "next/server";
+import { userService } from "./services/user.service";
+import { Roles } from "./constance/roles";
 
-export function proxy(request: NextRequest) {
-    
-    console.log(request.url)
+export async function proxy(request: NextRequest) {
+    const pathname = request.nextUrl.pathname
+
+    let isAuthinticated = false
+    let isAdmin = false
+    const { data } = await userService.getSession()
+
+    if (data) {
+        isAuthinticated = true
+        isAdmin = data.user.role === Roles.admin
+    }
+
+    if (!isAuthinticated) {
+        return NextResponse.redirect(new URL("/login", request.url))
+        // ! if the user does not login it will redirect him to login page
+    }
+
+    if (isAdmin && pathname.startsWith("/dashboard")) {
+        return NextResponse.redirect(new URL("/admin-dashboard", request.url))
+    }// ! When admin tries to access user Dashboard
+
+    if (!isAdmin && pathname.startsWith("/admin-dashboard")) {
+        return NextResponse.redirect(new URL("/dashboard", request.url))
+    } // ! When user tries to access Admin Dashboard
+
+    console.log(pathname)
 
     return NextResponse.next()
-
 }
 
 
 export const config = {
-    matcher: ["/dashboard"]
+    matcher: ["/dashboard","/dashboard/:path*","/admin-dashboard","/admin-dashboard/:path*"]
 }
