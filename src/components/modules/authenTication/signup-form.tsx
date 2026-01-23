3839a040d4054c34bf48a178d9157bfe187e9a8a
Ayon203008 <paypal572874@gmail.com>
2026-01-23 14:20:15 +0600

    tanstack query added to signup form

--- src/components/modules/authenTication/signup-form.tsx
+++ src/components/modules/authenTication/signup-form.tsx
@@ -1,20 +1,29 @@
-import { Button } from "@/components/ui/button"
+"use client";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/card";
+import { FieldGroup, FieldLabel } from "@/components/ui/field";
+
+import { Field, useForm } from "@tanstack/react-form";
 
 export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: async ({ value }) => {
+      console.log("Submit clicked", value);
+    },
+  });
+
   return (
     <Card {...props}>
       <CardHeader>
@@ -24,53 +33,49 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
+        <form
+          id="login-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            form.handleSubmit();
+          }}
+        >
           <FieldGroup>
-            <Field>
-              <FieldLabel htmlFor="name">Full Name</FieldLabel>
-              <Input id="name" type="text" placeholder="John Doe" required />
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="email">Email</FieldLabel>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-              <FieldDescription>
-                We&apos;ll use this to contact you. We will not share your email
-                with anyone else.
-              </FieldDescription>
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="password">Password</FieldLabel>
-              <Input id="password" type="password" required />
-              <FieldDescription>
-                Must be at least 8 characters long.
-              </FieldDescription>
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="confirm-password">
-                Confirm Password
-              </FieldLabel>
-              <Input id="confirm-password" type="password" required />
-              <FieldDescription>Please confirm your password.</FieldDescription>
-            </Field>
-            <FieldGroup>
-              <Field>
-                <Button type="submit">Create Account</Button>
-                <Button variant="outline" type="button">
-                  Sign up with Google
-                </Button>
-                <FieldDescription className="px-6 text-center">
-                  Already have an account? <a href="#">Sign in</a>
-                </FieldDescription>
-              </Field>
-            </FieldGroup>
+            <form.Field
+              name="name"
+              children={(field) => (
+                <div>
+                  <FieldLabel htmlFor={field.name}>Name</FieldLabel>
+                  <input  id={field.name} name={field.name} type="text" value={field.state.value} onChange={(e)=>field.handleChange(e.target.value)} />
+                </div>
+              )}
+            />
+            <form.Field
+              name="email"
+              children={(field) => (
+                <div>
+                  <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                  <input  id={field.name} name={field.name} type="email" value={field.state.value} onChange={(e)=>field.handleChange(e.target.value)} />
+                </div>
+              )}
+            />
+            <form.Field
+              name="password"
+              children={(field) => (
+                <div>
+                  <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                  <input  id={field.name} type="text" name={field.name} value={field.state.value} onChange={(e)=>field.handleChange(e.target.value)} />
+                </div>
+              )}
+            />
           </FieldGroup>
         </form>
       </CardContent>
+      <CardFooter className="flex justify-center">
+        <Button form="login-form" type="submit">
+          Submit
+        </Button>
+      </CardFooter>
     </Card>
-  )
+  );
 }
