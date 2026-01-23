6873a1401eedaa443a0ee947856eae7ad03219e5
Ayon203008 <paypal572874@gmail.com>
2026-01-23 10:45:36 +0600

    update

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client"
+import { getBlogs } from '@/actions/blog.action'
+import React, { useEffect, useState } from 'react'
 
-export default async function AboutPage() {
-  
-    await new Promise ((resolve)=>setTimeout(resolve,3000))
-    // throw new Error("Something went wrong")
+export default function AboutPage() {
+
+  const [data, setData] = useState<unknown>(null)
+
+  console.log(data)
+  useEffect(()=>{
+    (async()=>{
+      const {data}= await getBlogs()
+      setData(data)
+    })()
+  },[])
   return (
     <div> This is about page </div>
   )
--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -3,19 +3,24 @@ import BlogCard from "@/components/modules/homePage/BlogCard";
 import { blogService } from "@/services/blog.service";
 import { BlogPost } from "@/types";
 
-import React from "react";
+import React, { cache } from "react";
 
 export default async function Home() {
-
-  const {data}= await blogService.getBlogPosts()
-  console.log(data)
-
+  const { data } = await blogService.getBlogPosts(
+    {
+      isFeatured: false,
+    },
+    {
+     cache:"no-store",
+    },
+  );
+  console.log(data);
 
   return (
     <div className="grid grid-cols-5  mx-auto px-5 gap-5">
-      {
-        data?.data?.map((post:BlogPost)=><BlogCard key={post.id} post={post}/>)
-      }
+      {data?.data?.map((post: BlogPost) => (
+        <BlogCard key={post.id} post={post} />
+      ))}
     </div>
   );
 }
